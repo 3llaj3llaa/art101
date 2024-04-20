d108dfafd6267634aec161c105b4b1c808defd5f
3llaj3lla <166078869+3llaj3lla@users.noreply.github.com>
2024-04-19 18:46:53 -0700

    Lab 4
    Added pseudocode and explanations to Lab 4. Also touched up on my site.css to make things more cute

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - An implementation of Rock-Paper-Scissors
+// Ella Riuz <etruiz@ucsc.edu>
+// Date: 4/19/2024
 
 // Constants
 
@@ -19,3 +19,22 @@ function main() {
 
 // let's get this party started
 main();
+
+//Loop until the game is over
+  //Get the choice of Player 1 and Player 2
+  //Print Player 1's choice and Pring Player 2's choice
+  //Determine which Player won
+    //If Player 1 chooses Rock and Player 2 chooses Scissors, Player 1 wins
+    //If Player 1 chooses Scissors and Player 2 chooses Paper, Player 1 wins
+    //If Player 1 chooses Paper and Player 2 chooses Scissors, Player 1 wins
+    //If Player 2 chooses Rock and Player 1 chooses Scissors, Player 2 wins
+    //If Player 2 chooses Scissors and Player 1 chooses Paper, Player 2 wins
+    //If Player 2 chooses Paper and Player 1 chooses Rock, Player 2 wins
+    //If both players choose the same, its a tie
+  //Print the result of the round
+  //Ask if the players want to play another round
+//Print the final result of the game
+
+//We took the example given and the main steps provided by the example and inputted them into Bing Ai
+//to rephrase the pseudocode provided into more understandable terms for Sam and I.
+
