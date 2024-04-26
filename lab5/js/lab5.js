7b5cb77cf715a56a5635194b773fa4f355d233e8
3llaj3llaa <166079120+3llaj3llaa@users.noreply.github.com>
2024-04-25 19:47:03 -0700

    idk what is happening

---
+++ lab5/js/lab5.js
@@ -0,0 +1,46 @@
+// index.js - Lab 5 JavaScript Program
+// Author: Ella Ruiz <etruiz@ucsc.edu>
+// Date: 25 April
+
+
+// Define Variables
+
+make = "Mazda";
+model = "Miata";
+color = "Purple and White";
+year = 1990;
+
+
+// Calculate
+age = 2024 - year;
+
+
+// Output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "years<br>");
+
+
+
+
+
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
