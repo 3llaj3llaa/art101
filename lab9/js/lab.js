708785240e8c3f2318128211b8d9be6f754f5e91
3llaj3llaa <166079120+3llaj3llaa@users.noreply.github.com>
2024-05-12 01:00:50 -0700

    Lab 9
    All tasks and attempt at task X which was unsuccessful.

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,21 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 9 - Libraries and jQuery - it makes some buttons to modify elements on the page
+// Author: Ella Ruiz <etruiz@ucsc.edu>
+// jQuery must be loaded for this script to work
+// Date: 5/11/2024
 
-// Constants
+//add button to challenge section
+$("#challenge").append("<button id= 'button-challenge'>Click Me! </button>");
+//add a click listener to the challenge button
+$("#button-challenge").click(function(){
+    //add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special1");
+});
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//add button to problems section
+$("#problems").append("<button id= 'button-problems'>Click Me. </button>");
+//add a click listener
+$("#button-problems").click(function(){
+    //add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special2");
+});
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//add button to results section
+$("#results").append("<button id= 'button-results'>Click Me? </button>");
+//add a click listener
+$("#button-results").click(function(){
+   //add (or subtract) the "special" class to the section
+   $("#results").toggleClass("special3");
+});
 
-// let's get this party started
-main();
+
