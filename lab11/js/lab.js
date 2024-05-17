2be323eb7358fd5aab2acc56a03e236e50eb6599
GiacoWick <giacomocmatesic@gmail.com>
2024-05-17 12:30:51 -0700

    Lab 11
    Created a button that puts the users name in alphabetical order

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 11 - JavaScript Events and Forms
+// lab.js - This simple script gets a value from an input field and outputs a modified version.
+// Requirements: jQuery must be loaded for this script to work.
+// Author: Ella Ruiz <etruiz@ucsc.edu>
+// Date: May 17th, 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // Convert the string to an array and back again to sort it.
+  return inputString.split('').sort().join('');
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// click listener for button
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+  // sort it
+  userNameSorted = sortString(userName);
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
 
-// let's get this party started
-main();
