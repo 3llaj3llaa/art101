43c0282c5c351ed752c96a3a78376ea8765904ba
3llaj3llaa <166079120+3llaj3llaa@users.noreply.github.com>
2024-05-23 21:06:37 -0700

    lab 12 task X
    I changed the else if mods to include a house and description and moved the return to the bottom which includes both house and description in the results. In order to make this work I added more variables to the button's click function.

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -9,24 +9,35 @@
 function sortingHat(str) {
   len = str.length;
   mod = len % 4;
+  let house = "";
+  let description = "";
+
   if (mod == 0) {
-    return "Gryffindor"
+    house = "Gryffindor";
+    description = "Gryffindor house is where you would find the pluckiest and most daring students. The house colors are scarlet and gold, and the emblem a lion, and being placed here means you have demonstrated qualities like courage, bravery, and determination.";
   }
   else if (mod == 1) {
-    return "Ravenclaw"
+    house = "Ravenclaw";
+    description = "This is where you will find the brainiest students! The colors are blue and bronze, the emblem an eagle, and being placed here means you have demonstrated excellent wisdom, wit, and a skill for learning!";
   }
   else if (mod == 2) {
-    return "Slytherin"
+    house = "Slytherin";
+    description = "This house has a rather unfortunate reputation...The house colors are silver and green and the emblem is a serpent. Being placed in this house means you are likely ambitious, shrewd, and possibly even destined for greatness!";
   }
   else if (mod == 3) {
-    return "Hufflepuff"
+    house = "Hufflepuff";
+    description = "Here you will find the most trustworthy and hardworking students. The colors are yellow and black, and the emblem is a badger, you are likely very humble, have a strong moral compass, always work hard, and the most loyal friend!";
   }
+
+  return {house: house, description: description };
 }
 
 var myButton = document.getElementById("button");
 myButton.addEventListener("click", function () {
     var name = document.getElementById("input").value;
-    var house = sortingHat(name);
-    newText = "<p> The Sorting Hat has sorted you into " + house + "!</p>";
+    var result = sortingHat(name);
+    var house = result.house;
+    var description = result.description;
+    newText = "<p> The Sorting Hat has sorted you into " + house + "!</p><p>" + description + "</p>";
     document.getElementById("output").innerHTML = newText;
-})
+});
